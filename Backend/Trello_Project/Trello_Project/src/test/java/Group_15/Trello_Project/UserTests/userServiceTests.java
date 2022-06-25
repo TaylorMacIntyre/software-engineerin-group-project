0003a9058520724883dcbbe954adce1d9290b6a9
Taylor MacInytre <ty452018@dal.ca>
2022-06-24 21:30:41 -0300

    moved userService tests to their own package, added Mockito dependency

---
+++ Backend/Trello_Project/Trello_Project/src/test/java/Group_15/Trello_Project/UserTests/userServiceTests.java
@@ -0,0 +1,78 @@
+package Group_15.Trello_Project.UserTests;
+
+import Group_15.Trello_Project.*;
+import Group_15.Trello_Project.user.entity.UserModel;
+import Group_15.Trello_Project.user.repository.UserRepository;
+import Group_15.Trello_Project.user.service.UserServiceImplementation;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@ExtendWith(MockitoExtension.class)
+public class userServiceTests {
+    @Mock
+    @Autowired
+    private UserRepository userRepository;
+    @InjectMocks
+    private UserServiceImplementation userService;
+    private UserModel userModel;
+    String fName1 = "jane";
+    String fName2 = "john";
+    String lName = "doe";
+    String email1 = "myEmail1";
+    String email2 = "myEmail2";
+    String password1 = "Password1!";
+    String password2 = "Password2!";
+    String answer1 = "ans1";
+    String answer2 = "ans2";
+
+    @BeforeEach
+    public void setUp() throws EmailAlreadyRegisteredException {
+        userModel = new UserModel(fName1, lName, email1, password1, answer1);
+        userService.signUpUser(userModel);
+    }
+    @AfterEach
+    public void cleanUp(){
+        userRepository.deleteAll();
+    }
+    //SIGNUP
+    //Check email addy not already in db
+    @Test
+    public void testSignUp_emailAlreadyInDB() throws EmailAlreadyRegisteredException{
+        assertThrows(EmailAlreadyRegisteredException.class, ()->userModel = new UserModel(fName2, lName, email1, password1, answer1));
+    }
+
+
+    //LOGIN
+    //email doesnt exist
+    @Test
+    public void testLogin_emailAddressNotPresentInDB() throws EmailNotRegisteredException {
+        assertThrows(EmailNotRegisteredException.class, ()->userService.logInUser(email2, password1));
+    }
+
+    //check input password matches password in user model
+    @Test
+    public void testLogin_PWNotCorrect() throws IncorrectPasswordException {
+        assertThrows(IncorrectPasswordException.class, ()->userService.logInUser(email1, password2));
+    }
+
+    //FORGOT PW
+    //check security question answer is correct
+    @Test
+    public void testForgotPW_securityQAnswerNotCorrect() throws IncorrectSecurityAnswerException {
+        assertThrows(IncorrectSecurityAnswerException.class, ()->userService.updatePassword(email1, answer2, password1));
+    }
+
+    //check new pw != old pw
+    @Test
+    public void testForgotPW_newPWDoesNotEqualOldPW() throws NewPasswordSameAsOldPasswordException {
+        assertThrows(NewPasswordSameAsOldPasswordException.class, ()->userService.updatePassword(email1, answer1, password1));
+    }
+}
