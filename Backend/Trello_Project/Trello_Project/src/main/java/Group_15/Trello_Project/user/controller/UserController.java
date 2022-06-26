faef6c82d444fdd1486c6fcd062c364ac37ed438
Taylor MacInytre <ty452018@dal.ca>
2022-06-26 17:55:04 -0300

    added @CrossOrigin(origins = "http://localhost:3000") to all mappings, changed server.port to 3000

--- Backend/Trello_Project/Trello_Project/src/main/java/Group_15/Trello_Project/user/controller/UserController.java
+++ Backend/Trello_Project/Trello_Project/src/main/java/Group_15/Trello_Project/user/controller/UserController.java
@@ -7,24 +7,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/user")
 public class UserController {
 
     @Autowired
     UserServiceInterface userServiceInterface;
-
+    @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping("/save")
     public Integer signupUser(@RequestBody UserModel userModel) throws EmailAlreadyRegisteredException {
         return userServiceInterface.signUpUser(userModel);
     }
 
     //login?
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/login/{email}/{password}")
     public Integer loginUser(@PathVariable String email, @PathVariable String password) throws IncorrectPasswordException, EmailNotRegisteredException {
         return userServiceInterface.logInUser(email, password);
     }
 
     //forgot pw?
+    @CrossOrigin(origins = "http://localhost:3000")
     @PutMapping("/updatePW/{email}/{securityQAnswer}/{newPassword}")
     public boolean updatePassword(@PathVariable String email, @PathVariable String securityQAnswer, @PathVariable String newPassword) throws IncorrectSecurityAnswerException, EmailNotRegisteredException, NewPasswordSameAsOldPasswordException {
         return userServiceInterface.updatePassword(email, securityQAnswer, newPassword);
