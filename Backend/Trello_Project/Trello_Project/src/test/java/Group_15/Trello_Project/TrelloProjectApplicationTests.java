5151897f27f0f7687948a075538e7e34fe3682f3
Yuejia Zhang <yj998768@dal.ca>
2022-06-30 13:00:09 -0300

    Update Backend/Trello_Project/Trello_Project/src/test/java/Group_15/Trello_Project/TrelloProjectApplicationTests.java

--- Backend/Trello_Project/Trello_Project/src/test/java/Group_15/Trello_Project/TrelloProjectApplicationTests.java
+++ Backend/Trello_Project/Trello_Project/src/test/java/Group_15/Trello_Project/TrelloProjectApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TrelloProjectApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 
 }
