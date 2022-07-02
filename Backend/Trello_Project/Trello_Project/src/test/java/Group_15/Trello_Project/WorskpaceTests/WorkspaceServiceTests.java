0249863fae5db300e1db65f8a78ad64c05887c94
Ferwa Tariq <ferwa.tariq@dashhudson.com>
2022-07-02 20:32:50 -0300

    Change

--- Backend/Trello_Project/Trello_Project/src/test/java/Group_15/Trello_Project/WorskpaceTests/WorkspaceServiceTests.java
+++ Backend/Trello_Project/Trello_Project/src/test/java/Group_15/Trello_Project/WorskpaceTests/WorkspaceServiceTests.java
@@ -10,6 +10,29 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -41,4 +64,6 @@ public class WorkspaceServiceTests {
 
         assertNotNull(savedWorkspace);
     }
+
+
 }
