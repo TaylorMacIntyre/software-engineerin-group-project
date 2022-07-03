1fc761b975b36a3a30315de3bf4a1218db86b917
Kairui Liang <kr495028@dal.ca>
2022-07-03 16:17:07 -0300

    test

--- FrontEnd/group-project/src/components/Navigation2.js
+++ FrontEnd/group-project/src/components/Navigation2.js
@@ -3,11 +3,13 @@ import { Box, AppBar, Toolbar, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 
 function Navigation2() {
+    const uid = localStorage.getItem("uid");
+
     return (
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static">
                 <Toolbar>
-                    <Link to='/WorkSpace' style={{ textDecoration: 'none', color: 'white' }}>
+                    <Link to={'/WorkSpace/' + uid} style={{ textDecoration: 'none', color: 'white' }}>
                         <Typography variant="h6" component="div" sx={{ padding: '0 8px' }}>
                             WorkSpace
                         </Typography>
