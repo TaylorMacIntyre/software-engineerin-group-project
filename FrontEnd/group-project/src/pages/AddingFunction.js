f24dc4e10a597c7fdd0c49900c293c9be518b078
Katherine Arsenault <kt514867@dal.ca>
2022-07-08 18:57:21 -0300

    Revert "add an error detector"
    This reverts commit 49dbd612d7f11ec98548d811f3f384ca75c68179

--- FrontEnd/group-project/src/pages/AddingFunction.js
+++ FrontEnd/group-project/src/pages/AddingFunction.js
@@ -1,30 +1,19 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 import AddingForm from '../components/Addingmember';
-import { toast } from 'react-toastify';
+
 function AddingFunction() {
     const uid = localStorage.getItem("uid")
     const workid = localStorage.getItem("workid")
     const history = useHistory();
-    
     function AddingHandler(email){
         fetch(`http://localhost:9000/workspace/addUserToWorkspace/${workid}?email=` + email, {
             method: 'PUT',
-
             body: JSON.stringify(email),
             headers: {
                 'Content-Type': 'application/json'
             }
-        }).then((r) => {
-            if(r.ok) return r.text();
-            throw new Error("Failed")
-        })
-        .then(() => history.replace('/WorkSpace/'+ uid))
-        .catch(e => toast.error("Adding user failed"))
-        
-        
-        
-        
+        }).then(() => history.replace('/WorkSpace/'+ uid));
     }
 
     return(
