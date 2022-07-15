7c6b9f8202894ad5e71b37554fae58e497eb1256
Kairui Liang <kr495028@dal.ca>
2022-07-15 15:01:56 -0300

    finish create task

--- FrontEnd/group-project/src/Task/Viewtask.js
+++ FrontEnd/group-project/src/Task/Viewtask.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Grid, Card, CardContent, Typography, Button } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { Grid, Card, CardContent, Typography, Button, Stack } from '@mui/material';
+
 
 function ViewTask(props) {
 
@@ -8,9 +8,10 @@ function ViewTask(props) {
 
     return (
         <section style={{ marginTop: '32px' }}>
-            <Typography variant='h2' component='h2'>Task</Typography>
+            
             <Grid container spacing={2}>
-                {props.task.map((task) => {
+                
+            {props.task.map((task) => {
                     return (
                         <Grid item xs={12} sm={12} md={4} lg={4} key={task.id}>
                             <Card elevation={6}>
